8c0b500ff53a9cf61a249b6652b0bd3b23e9eb8a
omedsunriv <omareduardo74@gmail.com>
2022-09-13 16:38:29 -0400

    Updates

--- spa/assets/use-quasar.4631873d.js
+++
@@ -1,1 +0,0 @@
-import{v as a,aO as r}from"./index.d3912df2.js";function u(){return a(r)}export{u};
---
+++ spa/assets/use-quasar.85bfbbbd.js
@@ -0,0 +1,1 @@
+import{v as a,aO as r}from"./index.647a5766.js";function u(){return a(r)}export{u};
